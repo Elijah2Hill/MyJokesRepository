f17f3a9a3dd477866fc06ec5c5b0d386cbf8e854
Elijah2Hill <90879350+Elijah2Hill@users.noreply.github.com>
2021-10-18 20:48:42 -0400

    2 additional files

---
+++ dom.js
@@ -0,0 +1,11 @@
+const h1 = document.querySelector("h1");
+console.log(h1);
+h1.textContent = "My Jokes";
+
+const p = document.querySelector("#firstParagraph");
+console.log(p);
+p.textContent = "What should you call a person who has the same distinctive personality? You're the 'Goofy' one!";
+
+const p1 = document.querySelector("#secondParagraph");
+console.log(p1);
+p1.textContent = "What creature lives on top of your hair and can be tiny on all directions? Mickey Louse!";
